0b26d2587b673ac6f26e0f254f740e619dbaa0b1
Naufal <putper.siregar@gmail.com>
2024-05-05 09:53:54 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const server = Hapi.server({
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.send.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
